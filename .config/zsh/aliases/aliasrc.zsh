9d2761afbeba8d6c54f212d9d2da8e6eee728e2d
SisoroT <rtaylor6410@gmail.com>
2026-01-16 18:13:31 -0500

    remove some old crutch aliases

--- .config/zsh/aliases/aliasrc.zsh
+++ .config/zsh/aliases/aliasrc.zsh
@@ -1,17 +1,13 @@
 # => system commands
-alias cl="clear"
-alias ei="exit"
-alias l="exa -lha --git --icons --group-directories-first"
-alias ls="exa --icons"
-# alias l="ls -lhA --group-directories-first"
-# alias ls="ls --color"
+if command -v eza &> /dev/null; then
+    alias l="eza -lha --git --icons --group-directories-first"
+else
+    alias l="ls -lhA --group-directories-first --color=auto"
+fi
 alias grep="grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
-alias zz="z -"
 
 # => important programs
 alias vim="nvim"
-alias v="nvim"
-alias fm="vifm"
 
 # => scripts
 alias ratemir='export TMPFILE="$(mktemp)"; \
@@ -19,6 +15,3 @@ alias ratemir='export TMPFILE="$(mktemp)"; \
     rate-mirrors --save=$TMPFILE arch --max-delay=21600 \
       && sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist-backup \
       && sudo mv $TMPFILE /etc/pacman.d/mirrorlist'
-
-# => downloaded programs
-alias neo="neofetch"
